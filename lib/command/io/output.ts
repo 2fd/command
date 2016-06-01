744ce0157530c11895e02962cc5ba48dc96b4756
Fede Ramirez <i@2fd.me>
2016-05-31 22:48:21 -0300

    add ExpectOutput

--- lib/command/io/output.ts
+++ lib/command/io/output.ts
@@ -1,6 +1,41 @@
 import {InputInterface, OutputInterface, FormatterInterface} from '../../interfaces';
 import {Formatter, ColorFormatter} from './utils';
 
+
+/**
+ * Output from test
+ */
+type logger = (msj: string, ...replacements: Array<any>) => void;
+
+export class OuputExpected implements OutputInterface {
+
+    count = 0;
+    
+    expectations: Array<logger>
+
+    constructor(expectations: Array<logger>){
+        this.expectations = expectations;
+    }
+
+    log(msj: string, ...replacements: Array<any>) {
+        
+        let expect = this.expectations[this.count];
+        
+        if(typeof expect !== 'function')
+            throw new Error('Unexpected output');
+        
+        expect(msj, ...replacements);
+        this.count++;
+    }
+    
+    error(msj: string, ...replacements: Array<any>) {
+        this.log(msj, ...replacements);
+    }
+}
+
+/**
+ * Ignore all output
+ */
 export class DummyOutput implements OutputInterface {
     log(): void { }
     error(): void { }
