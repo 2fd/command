9b342c487d762f0db91eac57a897288bac30fee6
Fede Ramirez <i@2fd.me>
2016-06-02 00:31:01 -0300

    add RequireFlag, ValueFlag and ListValueFlag

--- lib/command/flags.ts
+++ lib/command/flags.ts
@@ -1,5 +1,6 @@
 import {
     InputInterface,
+    OutputInterface,
     FlagInterface
 } from '../interfaces';
 
@@ -85,6 +86,138 @@ export class HelpFlag extends BooleanFlag implements FlagInterface {
         description = 'Print this help'
     ) {
         super(name, list, description);
+    }
+}
+
+export class RequireFlag implements FlagInterface {
+
+    parsed: boolean;
+
+    flag: FlagInterface;
+
+    constructor(flag: FlagInterface) {
+        this.flag = flag;
+        this.parsed = false;
+    }
+
+    get name() {
+        return this.flag.name;
+    };
+
+    get description() {
+        return this.flag.description;
+    }
+
+    get list() {
+        return this.flag.list;
+    }
+
+    after(input: InputInterface, output: OutputInterface): void {
+        this.flag.after(input, output);
+
+        if (!this.parsed)
+            throw new Error(`Flag ${this.flag.name} (${this.flag.list.join(', ')}) is required`);
+    }
+
+    before(input: InputInterface, output: OutputInterface): void {
+        this.flag.before(input, output);
+    }
+
+    parse(input: InputInterface, output: OutputInterface): void {
+        this.flag.parse(input, output);
+    }
+}
+
+export class ValueFlag<T> extends FlagConstructor implements FlagInterface {
+
+    parser: (value: string) => T;
+
+    def: T;
+
+    constructor(
+        name: string,
+        list: Array<string>,
+        description: string,
+        parser?: (value: string) => T,
+        def?: T
+    ) {
+        super(name, list, description);
+        this.parser = parser;
+        this.def = def;
+    }
+
+    after(input: InputInterface): void { }
+
+    before(input: InputInterface): void {
+
+        if (typeof this.def !== 'undefined')
+            input.flags[this.name] = this.def;
+    }
+
+    parse(input: InputInterface): void {
+
+        if (input.argv.length === 0 || input.argv[0][0] === '-')
+            throw new Error(
+                `Flag ${this.name} (${this.list.join(', ')}) expect a value`
+            );
+
+        let val = input.argv.shift();
+
+        if (typeof this.parser === 'function')
+            input.flags[this.name] = this.parser(val);
+
+        else
+            input.flags[this.name] = val;
+    }
+}
+
+export class ListValueFlag<T> extends FlagConstructor implements FlagInterface {
+
+    parser: (value: string) => T;
+
+    def: Array<T>;
+
+    constructor(
+        name: string,
+        list: Array<string>,
+        description: string,
+        parser?: (value: string) => T,
+        def?: Array<T>
+    ) {
+        super(name, list, description);
+        this.parser = parser;
+
+        if (arguments.length < 5)
+            this.def = [];
+
+        else if (Array.isArray(def))
+            this.def = def;
+
+        else
+            throw new Error(`Flag ${this.name} (${this.list.join(', ')}) default value must be an Array`);
+    }
+
+    after(input: InputInterface): void { }
+
+    before(input: InputInterface): void {
+        input.flags[this.name] = this.def;
+    }
+
+    parse(input: InputInterface): void {
+
+        if (input.argv.length === 0 || input.argv[0][0] === '-')
+            throw new Error(
+                `Flag ${this.name} (${this.list.join(', ')}) expect a value`
+            );
+            
+        if(input.flags[this.name] === this.def)
+            input.flags[this.name] = [];
+
+        let val = input.argv.shift();
+        
+        input.flags[this.name].push(
+            typeof this.parser === 'function' ? this.parser(val) : val
+        );
     }
 }
 
--- test/lib/command/flags.test.ts
+++ test/lib/command/flags.test.ts
@@ -1,6 +1,27 @@
 import {expect} from 'chai';
-import {ArgvInput} from '../../../lib/command/io';
-import {NullFlag, BooleanFlag} from '../../../lib/command/flags';
+import {InputInterface, FlagInterface} from '../../../lib/interfaces';
+import {ArgvInput, DummyOutput} from '../../../lib/command/io';
+import {NullFlag, BooleanFlag, RequireFlag, ValueFlag, ListValueFlag} from '../../../lib/command/flags';
+
+function consume(flag: FlagInterface, input: InputInterface): InputInterface {
+
+    let output = new DummyOutput;
+
+    flag.before(input, output);
+
+    while (input.argv.length > 0) {
+        let f = input.argv.shift();
+
+        if (flag.list.indexOf(f) === -1)
+            throw Error(f + 'not definet in input');
+
+        flag.parse(input, output);
+    }
+
+    flag.after(input, output);
+
+    return input;
+}
 
 describe('./lib/command/flags', () => {
 
@@ -21,11 +42,7 @@ describe('./lib/command/flags', () => {
         it('define FALSE by default', () => {
 
             let input = new ArgvInput([]);
-            booleanFlag.before(input);
-            // booleanFlag.parse(input);
-            booleanFlag.after(input);
-
-            expect(input).to.be.deep.equal({
+            expect(consume(booleanFlag, input)).to.be.deep.equal({
                 argv: [],
                 exec: [],
                 flags: {
@@ -37,12 +54,8 @@ describe('./lib/command/flags', () => {
 
         it('define TRUE if is parsed', () => {
 
-            let input = new ArgvInput([]);
-            booleanFlag.before(input);
-            booleanFlag.parse(input);
-            booleanFlag.after(input);
-
-            expect(input).to.be.deep.equal({
+            let input = new ArgvInput(['--boolean']);
+            expect(consume(booleanFlag, input)).to.be.deep.equal({
                 argv: [],
                 exec: [],
                 flags: {
@@ -52,23 +65,204 @@ describe('./lib/command/flags', () => {
             });
         });
 
-        it('not change the input', () => {
+    });
 
-            let input = new ArgvInput(['param']);
-            booleanFlag.before(input);
-            booleanFlag.parse(input);
-            booleanFlag.after(input);
+    describe('ValueFlag', () => {
 
-            expect(input).to.be.deep.equal({
-                argv: ['param'],
+        let stringValueFlag = new ValueFlag<string>(
+            'string-flag', ['--string', '-s'], 'string flag'
+        );
+
+        it('expect a value', () => {
+
+            let otherFlagInput = new ArgvInput(['--string', '--otherFlag']);
+            expect(() => consume(stringValueFlag, otherFlagInput))
+                .throw(Error, 'Flag string-flag (--string, -s) expect a value');
+
+
+            let emptyValueInput = new ArgvInput(['--string']);
+            expect(() => consume(stringValueFlag, emptyValueInput))
+                .throw(Error, 'Flag string-flag (--string, -s) expect a value')
+
+        });
+
+        it('extract value', () => {
+
+            let input = new ArgvInput(['--string', 'random ;)']);
+            expect(consume(stringValueFlag, input)).to.be.deep.equal({
+                argv: [],
                 exec: [],
                 flags: {
-                    bool: true
+                    ['string-flag']: 'random ;)'
                 },
                 params: {},
             });
         });
 
+        let parserStringValueFlag = new ValueFlag<string>(
+            'parser-string-flag', ['--string', '-s'], 'string flag', (val: string) => '**' + val + '**'
+        );
+
+        it('parse value', () => {
+
+            let input = new ArgvInput(['--string', 'random ;)']);
+            expect(consume(parserStringValueFlag, input)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['parser-string-flag']: '**random ;)**'
+                },
+                params: {},
+            });
+        });
+
+        let defaultStringValueFlag = new ValueFlag<string>(
+            'default-string-flag', ['--string', '-s'], 'string flag', String, 'defaultString'
+        );
+
+        it('support default value', () => {
+
+            let input = new ArgvInput(['--string', 'random ;)']);
+            expect(consume(defaultStringValueFlag, input)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['default-string-flag']: 'random ;)'
+                },
+                params: {},
+            });
+
+            let emptyInput = new ArgvInput([]);
+            expect(consume(defaultStringValueFlag, emptyInput)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['default-string-flag']: 'defaultString'
+                },
+                params: {},
+            });
+        });
+    });
+
+    describe('ListValueFlag', () => {
+
+        let stringValueFlag = new ListValueFlag<string>(
+            'string-list-flag', ['--string', '-s'], 'string flag'
+        );
+
+        it('expect a value', () => {
+
+            let otherFlagInput = new ArgvInput(['--string', '--otherFlag']);
+            expect(() => consume(stringValueFlag, otherFlagInput))
+                .throw(Error, 'Flag string-list-flag (--string, -s) expect a value');
+
+
+            let emptyValueInput = new ArgvInput(['--string']);
+            expect(() => consume(stringValueFlag, emptyValueInput))
+                .throw(Error, 'Flag string-list-flag (--string, -s) expect a value')
+
+        });
+
+        it('extract values', () => {
+
+            let input = new ArgvInput(['--string', 'random ;)']);
+            expect(consume(stringValueFlag, input)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['string-list-flag']: ['random ;)']
+                },
+                params: {},
+            });
+
+            let inputMultiple = new ArgvInput(['--string', 'random ;)', '--string', 'random 2 ;)']);
+            expect(consume(stringValueFlag, inputMultiple)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['string-list-flag']: ['random ;)', 'random 2 ;)']
+                },
+                params: {},
+            });
+        });
+
+        let parserStringValueFlag = new ListValueFlag<string>(
+            'parser-string-list-flag', ['--string', '-s'], 'string flag', (val: string) => '**' + val + '**'
+        );
+
+        it('parse values', () => {
+
+            let input = new ArgvInput(['--string', 'random ;)']);
+            expect(consume(parserStringValueFlag, input)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['parser-string-list-flag']: ['**random ;)**']
+                },
+                params: {},
+            });
+
+            let inputMultiple = new ArgvInput(['--string', 'random ;)', '--string', 'random 2 ;)']);
+            expect(consume(parserStringValueFlag, inputMultiple)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['parser-string-list-flag']: ['**random ;)**', '**random 2 ;)**']
+                },
+                params: {},
+            });
+        });
+
+        let defaultStringValueFlag = new ListValueFlag<string>(
+            'default-string-list-flag', ['--string', '-s'], 'string flag', String, ['defaultString']
+        );
+
+        it('support default value', () => {
+
+            let input = new ArgvInput(['--string', 'random ;)']);
+            expect(consume(defaultStringValueFlag, input)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['default-string-list-flag']: ['random ;)']
+                },
+                params: {},
+            });
+
+            let emptyInput = new ArgvInput([]);
+            expect(consume(defaultStringValueFlag, emptyInput)).to.be.deep.equal({
+                argv: [],
+                exec: [],
+                flags: {
+                    ['default-string-list-flag']: ['defaultString']
+                },
+                params: {},
+            });
+        });
+
+        it('default value must be an array', () => {
+
+            let def: any = 'defaultString';
+
+            expect(() => (
+                new ListValueFlag(
+                    'default-string-list-flag', ['--string', '-s'], 'string flag', String, def
+                )
+            )).throw(Error, 'Flag default-string-list-flag (--string, -s) default value must be an Array')
+
+        })
+    });
+
+    describe('RequireFlag', () => {
+
+        let requireFlag = new RequireFlag( new ValueFlag('require-string-flag', ['--req', '-r'], 'string value') );
+        
+        it('must be require', () => {
+            
+            let input = new ArgvInput([]);
+            expect(() => consume(requireFlag, input))
+                .throw(Error, 'Flag require-string-flag (--req, -r) is required');
+        })
     });
 
 });
